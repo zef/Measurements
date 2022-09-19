527481fa4d5fb76f1d856738a5e79f15b20089fc
Zef Houssney <zef@zef.studio>
2022-09-19 13:45:53 -0600

    Add playground.

---
+++ Measurements.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let grams = UnitMass.grams
+let pounds = UnitMass.pounds
+
+//UnitMass(symbol: pounds.symbol)
+
+var measurement = Measurement(value: 1900, unit: grams)
+
+measurement + measurement
+
+measurement.converted(to: pounds)
+measurement
+
+let feet = UnitLength.feet
+
+let width = Measurement(value: 14, unit: feet)
+let length = Measurement(value: 10, unit: feet)
+
+
+//if let data = try? NSKeyedArchiver.archivedData(withRootObject: width, requiringSecureCoding: false) {
+//    data
+//    print(data)
+//    let newWidth = try? NSKeyedUnarchiver.unarchivedObject(ofClass: Measurement, from: data)
+//}
+
